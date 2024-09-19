2bda3df7216e217257d828285fb3f0120c845b16
Waquar <admin@waquar.com>
2024-09-19 12:33:45 +0530

    fix basic contents

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 st.set_page_config(
     page_title="Picnic Pro",
-    page_icon="🌍",
+    page_icon="⛱️",
     layout="wide",
     initial_sidebar_state="expanded"
 )
@@ -31,67 +31,68 @@ with col1:
     location = st.selectbox("Location", locations)
 
 with col2:    
-    number_of_days = st.number_input("Number of Days", min_value= 1, step= 1)
+    number_of_days = st.number_input("Number of Days", min_value= 0, step= 1)
     
 with col3:    
-    budget = st.number_input("Budget")
+    budget = st.number_input("Budget", min_value=0, step= 1)
 
+# TODO: will use model on click here...
 if st.button("GENERATE PLANS"):
     st.write(f"Generating plans for {location} for {number_of_days} days within a budget of {budget}...")
 
 st.write("---")
 st.write("## Suggested Activities")
+with st.container():
+    left_col, main_col, right_col = st.columns([1,8,1])  
 
-left_col, main_col, right_col = st.columns([1,8,1])  
+    with main_col:
+        with st.container():
+            col1, col2, col3 = st.columns(3)
 
-with main_col:
-    with st.container():
-        col1, col2, col3 = st.columns(3)
+            with col1:
+                if os.path.isfile(eduTourImg):
+                    image = Image.open(eduTourImg)
+                    st.image(image, width=300)
+                    st.markdown(
+                        """
+                        <div style="text-align: center;">
+                            <h4 style="font-size:18px; font-weight:600;">Educational Tour</h4>
+                        </div>
+                        """,
+                        unsafe_allow_html=True
+                    )     
+                else:
+                    st.error(f"Unable to load the file: {eduTourImg}")
 
-        with col1:
-            if os.path.isfile(eduTourImg):
-                image = Image.open(eduTourImg)
-                st.image(image, width=300)
-                st.markdown(
-                    """
-                    <div style="text-align: center;">
-                        <h4 style="font-size:18px; font-weight:600;">Educational Tour</h4>
-                    </div>
-                    """,
-                    unsafe_allow_html=True
-                )     
-            else:
-                st.error(f"Unable to load the file: {eduTourImg}")
+            with col2:
+                if os.path.isfile(funActivityImg):
+                    image = Image.open(funActivityImg)
+                    st.image(image, width= 300)  
+                    st.markdown(
+                        """
+                        <div style="text-align: center;">
+                            <h4 style="font-size:18px; font-weight:600;">Fun Tour</h4>
+                        </div>
+                        """,
+                        unsafe_allow_html=True
+                    )        
+                else:
+                    st.error(f"Unable to load the file: {funActivityImg}")
 
-        with col2:
-            if os.path.isfile(funActivityImg):
-                image = Image.open(funActivityImg)
-                st.image(image, width= 300)  
-                st.markdown(
-                    """
-                    <div style="text-align: center;">
-                        <h4 style="font-size:18px; font-weight:600;">Fun Activities</h4>
-                    </div>
-                    """,
-                    unsafe_allow_html=True
-                )        
-            else:
-                st.error(f"Unable to load the file: {funActivityImg}")
-
-        with col3:
-            if os.path.isfile(eventsImg):
-                image = Image.open(eventsImg)
-                st.image(image, width= 300)  
-                st.markdown(
-                    """
-                    <div style="text-align: center;">
-                        <h4 style="font-size:18px; font-weight:600;">Events For Children</h4>
-                    </div>
-                    """,
-                    unsafe_allow_html=True
-                )            
-            else:
-                st.error(f"Unable to load the file: {eventsImg}")
+            with col3:
+                if os.path.isfile(eventsImg):
+                    image = Image.open(eventsImg)
+                    st.image(image, width= 300)  
+                    st.markdown(
+                        """
+                        <div style="text-align: center;">
+                            <h4 style="font-size:18px; font-weight:600;">Events For Children</h4>
+                        </div>
+                        """,
+                        unsafe_allow_html=True
+                    )            
+                else:
+                    st.error(f"Unable to load the file: {eventsImg}")
 
 st.write("### Get in Touch")
 col1, col2 = st.columns(2)
